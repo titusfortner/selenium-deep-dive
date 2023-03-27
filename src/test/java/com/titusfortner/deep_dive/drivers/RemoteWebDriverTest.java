c9bb733514cffc1a4dd28e58d4bbf21528a5c1b8
titusfortner <titusfortner@gmail.com>
2023-03-27 14:27:06 -0500

    Augment Remote Driver

--- src/test/java/com/titusfortner/deep_dive/drivers/RemoteWebDriverTest.java
+++ src/test/java/com/titusfortner/deep_dive/drivers/RemoteWebDriverTest.java
@@ -1,7 +1,15 @@
 package test.java.com.titusfortner.deep_dive.drivers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumNetworkConditions;
+import org.openqa.selenium.chromium.HasNetworkConditions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.HttpCommandExecutor;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.http.ClientConfig;
@@ -9,6 +17,8 @@ import test.java.com.titusfortner.deep_dive.BaseTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RemoteWebDriverTest extends BaseTest {
     URL serverUrl = new URL("http://localhost:4444/");
@@ -37,5 +47,39 @@ public class RemoteWebDriverTest extends BaseTest {
         ClientConfig config = ClientConfig.defaultConfig().baseUrl(serverUrl);
         HttpCommandExecutor executor = new HttpCommandExecutor(config);
         driver = new RemoteWebDriver(executor, new ChromeOptions());
+    }
+
+    @Test
+    public void augmentNetworkConditions() {
+        driver = new RemoteWebDriver(new ChromeOptions());
+        driver = new Augmenter().augment(driver);
+
+        ChromiumNetworkConditions networkConditions = new ChromiumNetworkConditions();
+        networkConditions.setOffline(true);
+        ((HasNetworkConditions) driver).setNetworkConditions(networkConditions);
+
+        try {
+            driver.get("https://www.selenium.dev");
+            Assertions.fail("If Network is set to be offline, any navigation should throw an exception");
+        } catch (WebDriverException ex) {
+            ((HasNetworkConditions) driver).setNetworkConditions(new ChromiumNetworkConditions());
+        }
+
+        Assertions.assertDoesNotThrow(() -> driver.get("https://www.selenium.dev"));
+    }
+
+    @Test
+    @Disabled("Example Only")
+    public void buildRemoteWebDriver() {
+        Map<String, Object> sauceOptions = new HashMap<>();
+
+        driver = RemoteWebDriver.builder()
+                .address("https://ondemand.us-west-1.saucelabs.com/wd/hub")
+                .oneOf(new ChromeOptions(), new EdgeOptions())
+                .setCapability("sauce:options", sauceOptions)
+                .config(ClientConfig.defaultConfig())
+                .authenticateAs(new UsernameAndPassword("Username", "Password"))
+                // .withDriverService(ChromeDriverService.createDefaultService()) --> not used with address
+                .build();
     }
 }
