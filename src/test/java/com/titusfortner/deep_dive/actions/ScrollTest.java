237cc6931bbe679cba8f346a3be173dab0bcc87c
titusfortner <titusfortner@gmail.com>
2023-03-27 16:42:32 -0500

    Scrolling

---
+++ src/test/java/com/titusfortner/deep_dive/actions/ScrollTest.java
@@ -0,0 +1,102 @@
+package test.java.com.titusfortner.deep_dive.actions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput;
+import test.java.com.titusfortner.deep_dive.BaseTestChrome;
+
+public class ScrollTest extends BaseTestChrome {
+    @BeforeEach
+    public void setup() {
+        startDriver();
+    }
+
+    @Test
+    public void shouldScrollToElement() {
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+
+        WebElement element = driver.findElement(By.tagName("iframe"));
+        new Actions(driver)
+                .scrollToElement(element)
+                .perform();
+
+        Assertions.assertTrue(inViewport(element));
+    }
+
+    @Test
+    public void shouldScrollFromViewportByGivenAmount() {
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+
+        WebElement footer = driver.findElement(By.tagName("footer"));
+        int deltaY = footer.getRect().y;
+        new Actions(driver)
+                .scrollByAmount(0, deltaY)
+                .perform();
+
+        Assertions.assertTrue(inViewport(footer));
+    }
+
+    @Test
+    public void shouldScrollFromElementByGivenAmount() {
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+
+        WebElement iframe = driver.findElement(By.tagName("iframe"));
+        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin.fromElement(iframe);
+        new Actions(driver)
+                .scrollFromOrigin(scrollOrigin, 0, 200)
+                .perform();
+
+        driver.switchTo().frame(iframe);
+        WebElement checkbox = driver.findElement(By.name("scroll_checkbox"));
+        Assertions.assertTrue(inViewport(checkbox));
+    }
+
+    @Test
+    public void shouldScrollFromElementByGivenAmountWithOffset() {
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+
+        WebElement footer = driver.findElement(By.tagName("footer"));
+        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin
+                .fromElement(footer, 0, -50);
+        new Actions(driver)
+                .scrollFromOrigin(scrollOrigin,0, 200)
+                .perform();
+
+        WebElement iframe = driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(iframe);
+        WebElement checkbox = driver.findElement(By.name("scroll_checkbox"));
+        Assertions.assertTrue(inViewport(checkbox));
+    }
+
+    @Test
+    public void shouldScrollFromViewportByGivenAmountFromOrigin() {
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame.html");
+
+        WheelInput.ScrollOrigin scrollOrigin = WheelInput.ScrollOrigin
+                .fromViewport(10, 10);
+        new Actions(driver)
+                .scrollFromOrigin(scrollOrigin, 0, 200)
+                .perform();
+
+        WebElement iframe = driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(iframe);
+        WebElement checkbox = driver.findElement(By.name("scroll_checkbox"));
+        Assertions.assertTrue(inViewport(checkbox));
+    }
+
+    private boolean inViewport(WebElement element) {
+
+        String script =
+                "for(var e=arguments[0],f=e.offsetTop,t=e.offsetLeft,o=e.offsetWidth,n=e.offsetHeight;\n"
+                        + "e.offsetParent;)f+=(e=e.offsetParent).offsetTop,t+=e.offsetLeft;\n"
+                        + "return f<window.pageYOffset+window.innerHeight&&t<window.pageXOffset+window.innerWidth&&f+n>\n"
+                        + "window.pageYOffset&&t+o>window.pageXOffset";
+
+        return (boolean) ((JavascriptExecutor) driver).executeScript(script, element);
+    }
+}
