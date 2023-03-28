dc8426bd4f37b982795ede6faf5e98b07952fe36
titusfortner <titusfortner@gmail.com>
2023-03-28 01:01:15 -0500

    Abstract Waits

---
+++ src/test/java/com/titusfortner/deep_dive/demo/pages/ElementValidationException.java
@@ -0,0 +1,7 @@
+package test.java.com.titusfortner.deep_dive.demo.pages;
+
+public class ElementValidationException extends RuntimeException {
+    public ElementValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
