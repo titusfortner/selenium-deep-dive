7ce1f3a927a153863fed703f6c38f9e59c734518
titusfortner <titusfortner@gmail.com>
2023-03-28 01:12:05 -0500

    Wrap WebElement

---
+++ src/test/java/com/titusfortner/deep_dive/demo/elements/Element.java
@@ -0,0 +1,59 @@
+package test.java.com.titusfortner.deep_dive.demo.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import test.java.com.titusfortner.deep_dive.demo.pages.ElementValidationException;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class Element {
+    private final By locator;
+    private final WebDriverWait wait;
+    private WebElement cachedElement;
+
+    public Element(WebDriver driver, By locator) {
+        this.locator = locator;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    public boolean isDisplayed() {
+        locate();
+        return cachedElement.isDisplayed();
+    }
+
+    public void sendKeys(String value) {
+        locate();
+        run(() -> cachedElement.sendKeys(value), "send keys to element at: " + locator);
+    }
+
+    public void click() {
+        locate();
+        run(() -> cachedElement.click(), "click element at: " + locator);
+    }
+
+    private void locate() {
+        this.cachedElement = (WebElement) wait.until((Function<WebDriver, Object>) d -> d.findElement(locator));
+    }
+
+    private void run(Runnable block, String message) {
+        long startTime = System.currentTimeMillis();
+        while (true) {
+            try {
+                block.run();
+                break;
+            } catch (NoSuchElementException | ElementNotInteractableException e) {
+                long currentTime = System.currentTimeMillis();
+                Duration duration = Duration.ofMillis(currentTime - startTime);
+
+                if (duration.compareTo(Duration.ofSeconds(20)) > 0) {
+                    throw new ElementValidationException("Unable to " + message + " after " + duration + " seconds", e);
+                }
+            }
+        }
+    }
+}
--- src/test/java/com/titusfortner/deep_dive/demo/pages/FinishPage.java
+++ src/test/java/com/titusfortner/deep_dive/demo/pages/FinishPage.java
@@ -2,17 +2,18 @@ package test.java.com.titusfortner.deep_dive.demo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import test.java.com.titusfortner.deep_dive.demo.elements.Element;
 
 public class FinishPage extends BasePage {
     public static final String URL = "https://www.saucedemo.com/checkout-complete.html";
 
-    private final By completeText = By.className("complete-text");
+    private final Element completeText = new Element(driver, By.className("complete-text"));
 
     public FinishPage(WebDriver driver) {
         super(driver);
     }
 
     public boolean isComplete() {
-        return driver.findElement(completeText).isDisplayed();
+        return completeText.isDisplayed();
     }
 }
