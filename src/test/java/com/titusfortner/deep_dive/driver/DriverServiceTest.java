68f9004f735740f93a60262a24c24c13673a66e3
titusfortner <titus.fortner@gmail.com>
2025-03-23 17:51:49 +0100

    driver service class examples

---
+++ src/test/java/com/titusfortner/deep_dive/driver/DriverServiceTest.java
@@ -0,0 +1,47 @@
+package com.titusfortner.deep_dive.driver;
+
+import com.titusfortner.deep_dive.TestBase;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverService;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+public class DriverServiceTest extends TestBase {
+  @Test
+  public void chrome() throws IOException {
+    File logFile = File.createTempFile("chromedriver", "log");
+
+    ChromeDriverService service =
+        new ChromeDriverService.Builder()
+            .withVerbose(true)
+            .withLogFile(logFile)
+            .withReadableTimestamp(true)
+            .build();
+    driver = new ChromeDriver(service);
+
+    String output = Files.readAllLines(Paths.get(logFile.toString())).toString();
+
+    String expectedText = "[DEBUG]";
+    Assertions.assertTrue(output.contains(expectedText));
+  }
+
+  @Test
+  public void firefox() throws IOException {
+    System.clearProperty("webdriver.firefox.logfile");
+    File logFile = File.createTempFile("geckodriver", "log");
+
+    FirefoxDriverService service = new GeckoDriverService.Builder().withLogFile(logFile).build();
+    driver = new FirefoxDriver(service);
+
+    String output = Files.readAllLines(Paths.get(logFile.toString())).toString();
+    String expectedText = "Marionette\tINFO";
+    Assertions.assertTrue(output.contains(expectedText));
+  }
+}
