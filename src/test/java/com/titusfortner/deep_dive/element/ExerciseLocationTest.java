2ed69c543b60c4917988abffb41319acc89cb597
titusfortner <titus.fortner@gmail.com>
2025-03-24 13:03:12 +0100

    Exercise Answers: Advanced CSS Location

--- src/test/java/com/titusfortner/deep_dive/element/ExerciseLocationTest.java
+++ src/test/java/com/titusfortner/deep_dive/element/ExerciseLocationTest.java
@@ -17,16 +17,16 @@ public class ExerciseLocationTest extends TestBase {
   public void locateZipCodeField() {
     driver.get("https://www.saucedemo.com/v1/checkout-step-one.html");
 
-    String cssDescendant = "";
-    String cssDirectChild = "";
-    String cssLastChild = "";
-    String cssChildFromEnd = "";
-    String cssChildFromBeginning = "";
-    String cssAttributeStartsWith = "";
-    String cssAttributeEndsWith = "";
-    String cssAttributeContains = "";
-    String cssNextSibling = "";
-    String cssSubsequentSibling = "";
+    String cssDescendant = "#checkout_info_container #postal-code";
+    String cssDirectChild = ".checkout_info > #postal-code";
+    String cssLastChild = "input:last-child";
+    String cssChildFromEnd = "input:nth-last-child(1)";
+    String cssChildFromBeginning = "input:nth-child(3)";
+    String cssAttributeStartsWith = "[data-test^=post]";
+    String cssAttributeEndsWith = "[data-test$=ode]";
+    String cssAttributeContains = "[data-test*=alC]";
+    String cssNextSibling = "[data-test=lastName] + [data-test=postalCode]";
+    String cssSubsequentSibling = "[data-test=firstName] ~ [data-test=postalCode]";
 
     // IGNORE THE DETAILS OF THE ASSERTIONS BELOW
     // ALL YOU NEED TO KNOW IS THAT IF YOU RUN THIS TEST
