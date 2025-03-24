223ed99717ce5d8881758a88bca8d867658cb80a
titusfortner <titus.fortner@gmail.com>
2025-03-24 14:47:51 +0100

    decorator and listener examples

---
+++ src/test/java/com/titusfortner/deep_dive/support/CustomDecorator.java
@@ -0,0 +1,29 @@
+package com.titusfortner.deep_dive.support;
+
+import java.lang.reflect.Method;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.decorators.Decorated;
+import org.openqa.selenium.support.decorators.DefaultDecorated;
+import org.openqa.selenium.support.decorators.WebDriverDecorator;
+
+public class CustomDecorator extends WebDriverDecorator<WebDriver> {
+
+  @Override
+  public Decorated<WebElement> createDecorated(WebElement original) {
+    return new DefaultDecorated(original, this) {
+      @Override
+      public Object call(Method method, Object[] args) throws Throwable {
+        String methodName = method.getName();
+        if ("click".equals(methodName)) {
+          JavascriptExecutor executor = (JavascriptExecutor) getDecoratedDriver().getOriginal();
+          executor.executeScript("arguments[0].click()", getOriginal());
+          return null;
+        } else {
+          return super.call(method, args);
+        }
+      }
+    };
+  }
+}
