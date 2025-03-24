9ffe3eb78ec66a4450b811b9953bb989c5f66869
titusfortner <titus.fortner@gmail.com>
2025-03-24 09:06:14 +0100

    add basic logging

--- src/test/java/com/titusfortner/deep_dive/WorkingTest.java
+++ src/test/java/com/titusfortner/deep_dive/WorkingTest.java
@@ -1,5 +1,7 @@
 package com.titusfortner.deep_dive;
 
+import com.titusfortner.logging.SeleniumLogger;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +9,12 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WorkingTest {
+  /** Turn on logging for Selenium for all tests in this class */
+  @BeforeAll
+  public static void enableLogging() {
+    SeleniumLogger.enable();
+  }
+
   /** Verify that Selenium can drive each of these browsers */
   @Test
   public void validateChrome() {
