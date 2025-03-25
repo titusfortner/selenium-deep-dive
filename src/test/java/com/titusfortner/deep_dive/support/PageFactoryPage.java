b3008d4a076b0c342ebb0900e29415b5e9676e5d
titusfortner <titus.fortner@gmail.com>
2025-03-25 20:38:06 +0100

    Page Factory example of why not to use it

---
+++ src/test/java/com/titusfortner/deep_dive/support/PageFactoryPage.java
@@ -0,0 +1,49 @@
+package com.titusfortner.deep_dive.support;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageFactoryPage {
+  WebDriverWait wait;
+
+  public PageFactoryPage(WebDriver driver) {
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    PageFactory.initElements(driver, this);
+  }
+
+  @FindBy(id = "my-text-id")
+  WebElement textTextField;
+
+  @FindBy(name = "my-password")
+  WebElement passwordTextField;
+
+  @FindBy(tagName = "button")
+  WebElement submitButton;
+
+  public void submitForm(String text, String password) {
+    textTextField.sendKeys(text);
+    passwordTextField.sendKeys(password);
+    submitButton.click();
+  }
+
+  public void click(WebElement element) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
+    element.click();
+  }
+
+  public void sendKeys(WebElement element, String keys) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
+    element.sendKeys(keys);
+  }
+
+  public void synchronizedSubmitForm(String text, String password) {
+    sendKeys(textTextField, text);
+    sendKeys(passwordTextField, password);
+    click(submitButton);
+  }
+}
